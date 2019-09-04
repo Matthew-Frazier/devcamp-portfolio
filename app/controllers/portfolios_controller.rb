e4ac4da114cfab4c1d0c99a06c03e9f024ab7739
Matthew Frazier <matthewfrazier1988@gmail.com>
2019-09-04 17:24:51 -0400

    Implemented edit and update functionality

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,6 +1,11 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio_item, only: [:show, :edit, :update, :destroy] 
+
   def index
     @portfolio_items = Portfolio.all
+  end
+
+  def show
   end
 
   def new
@@ -15,6 +20,19 @@ class PortfoliosController < ApplicationController
         format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
       else
         format.html { render :new }
+      end
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @portfolio_item.update(portfolio_params)
+        format.html { redirect_to portfolios_path, notice: 'Your portfolio item was successfully updated.' }
+      else
+        format.html { render :edit }
       end
     end
   end
